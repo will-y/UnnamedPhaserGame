42f319c2f5dc28a1642ca6e0c8da5782587bb7b4
Will Yelton <willhyelton@gmail.com>
2022-06-11 21:07:39 -0400

    Changes arrow keys to wasd

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -35,8 +35,14 @@ class GameScene extends Scene {
         });
 
         // Create a helper object for our arrow keys
-        this.cursors = this.input.keyboard.createCursorKeys();
+        // this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.cursors = this.input.keyboard.addKeys(
+            {
+                up:Phaser.Input.Keyboard.KeyCodes.W,
+                down:Phaser.Input.Keyboard.KeyCodes.S,
+                left:Phaser.Input.Keyboard.KeyCodes.A,
+                right:Phaser.Input.Keyboard.KeyCodes.D});
         // Coin things
         const coins = this.physics.add.group();
         coins.create(200, 200, "coin");
