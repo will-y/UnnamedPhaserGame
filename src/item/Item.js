32c2247006a1d75aa4d6679b03b102cd8106685b
Will Yelton <willhyelton@gmail.com>
2022-06-16 23:22:18 -0400

    Adds item class

---
+++ src/item/Item.js
@@ -0,0 +1,11 @@
+/**
+ * Base class for all items
+ * This will just be item data, on use effects...
+ * Will NOT be the entity version of the item
+ * That will normally be a pickup entity
+ * Not all items need an item entity
+ * Items are the things that are stored in the inventory
+ */
+class Item {
+
+}
