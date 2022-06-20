6f49110b37fe2f040450f455cd49137b844f22d8
Will Yelton <willhyelton@gmail.com>
2022-06-20 10:26:21 -0400

    Player can pickup swords

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -14,6 +14,7 @@ class BootScene extends Scene {
         // Register Items (Maybe make this JSON later)
         // Maybe do the same thing with entities later
         ItemRegistry.register(new Item("coin", "other"));
+        ItemRegistry.register(new Item("sword", "primary"));
     }
 
     create() {
